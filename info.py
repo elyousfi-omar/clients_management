77f05487023b6b95f8213302f1f4a2892fad69a9
Omar El Yousfi <49985464+elyousfi-omar@users.noreply.github.com>
2021-09-18 19:07:45 +0100

    Add files via upload

---
+++ info.py
@@ -0,0 +1,9 @@
+print('''
+The repository is devided to three sections:
+* template contains html files
+* css contains the css of the html files
+** app.py is a python (flask) file that handles requests and data in general
+''')
+print('FLASK CRUD APPLICATION')
+print('#######################')
+print('OMAR EL YOUSFI')
